f806d36c1e99b6a3c0e0271e3a2b4e808e8e10d7
Abe Samaniego <abe4phx@gmail.com>
2026-07-15 18:10:17 -0600

    Finalize StoreKit dependencies and Capacitor configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.betteropnr',
+  appId: 'com.abe.BetterOpnr',
   appName: 'BetterOpnr',
   webDir: 'dist',
   server: {
